1e1d48383044eab7285a92c67eb092211d5a9e9c
John Nevins <jnevins@spotxchange.com>
2016-05-27 21:10:56 -0600

    User create service

--- server.js
+++ server.js
@@ -3,13 +3,16 @@
  */
 var express = require('express');
 var expressConfig = require('./server/config/express');
+var mongooseConfig = require('./server/config/mongoose');
 var routeConfig = require('./server/config/routes');
 
 var envconf = require('./server/config/envconf');
 var app = express();
 
 expressConfig(app, envconf);
+mongooseConfig(envconf);
 routeConfig(app, envconf);
 
 app.listen(envconf.port);
+//TODO: use Winston
 console.log('Listening on port ' + envconf.port + '...');
