641ba681e7463bfdda090df79c81080dd8ee3fe4
John Nevins <jnevins@spotxchange.com>
2016-05-28 08:26:26 -0600

    Production env conf

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 /**
  * Created by jnevins on 5/18/16.
  */
+var environment = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var express = require('express');
 var expressConfig = require('./server/config/express');
 var mongooseConfig = require('./server/config/mongoose');
 var routeConfig = require('./server/config/routes');
 
-var envconf = require('./server/config/envconf');
+var envconf = require('./server/config/envconf')[environment];
 var app = express();
 
 expressConfig(app, envconf);
