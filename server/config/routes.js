d4a6c199d522e70b8f34569d49ba586a46ba0946
John Nevins <jnevins@spotxchange.com>
2016-05-27 22:38:02 -0600

    Login Page Styling

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,12 @@
 /**
  * Created by jnevins on 5/18/16.
  */
-var path = require('path');
+var path = require('path'),
+    userService = require('../services/userService');
 
 module.exports = function(app, envconf) {
+
+    app.post('/api/user', userService.createUser);
 
     app.get('/', function(request, response) {
         response.sendFile(path.join(envconf.rootPath + 'views/index.html'));
