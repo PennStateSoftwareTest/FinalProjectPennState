fa361a2775ee0a33a03d17454f1db74c729120a1
Joshua Victor Sherman <joshua.v.sherman@gmail.com>
2016-06-04 19:42:33 -0400

    last tests for today

---
+++ test/config/AdminUserTests.js
@@ -0,0 +1,8 @@
+//given that user status as Admin type has been verified
+//given that login is successfully
+//expect that landing page for Admin displays
+//expect a sessionID that auto times out after 15 minutes
+//expect a form with the following actions
+  //delete a user account
+  //modify any field in a user accout with a click 'Update' button
+  //suspend a user acccount
