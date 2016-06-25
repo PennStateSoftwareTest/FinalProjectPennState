e5e8a8ed5910ccf5f8fa8333297c6093a2cddadf
Joshua Victor Sherman <joshua.v.sherman@gmail.com>
2016-06-25 13:18:51 -0400

    venuesgit add .

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,6 +7,9 @@ import {DashboardComponent} from "./dashboard/dashboard.component";
 import {LoginComponent} from "./login/login.component";
 import {NewUserComponent} from "./newuser/newuser.component";
 import {ForgotPasswordComponent} from "./forgotpassword/forgotpassword.component";
+import {Dashboard_VMComponent} from "./dashboard_vm/dashboard_vm.component";
+import {CreateVenue} from "./create_venue/create_venue.component";
+import {ExistingVenue} from "./existing_venue/existing_venue.component";
 
 //TODO: add routes and auth service
 
@@ -39,6 +42,21 @@ import {ForgotPasswordComponent} from "./forgotpassword/forgotpassword.component
         path: '/forgotpassword',
         name: 'ForgotPassword',
         component: ForgotPasswordComponent
+    },
+    {
+        path: '/dashboard_vm',
+        name: 'DashboardVM',
+        component: Dashboard_VMComponent
+    },
+    {
+        path: '/createvenue',
+        name: 'CreateVenue',
+        component: CreateVenue
+    },
+    {
+        path: '/existing_venue',
+        name: 'ExistingVenue',
+        component: ExistingVenue
     }
 ])
 export class AppComponent {
