db34f524db996117a016b0772bc3dca5a6da45db
Josh Sherman <joshua.v.sherman@gmail.com>
2016-06-26 13:56:04 -0400

    Update envconf.js

--- server/config/envconf.js
+++ server/config/envconf.js
@@ -6,12 +6,12 @@ var rootPath = path.join(__dirname + '/../../server/');
 
 module.exports = {
     development: {
-        db: 'mongodb://scrs:R0ckOn!$$@ds011298.mlab.com:11298/scrs',
+        db: process.env.MONGODB_URI,
         rootPath: rootPath,
         port: process.env.PORT || 3000
     },
     production: {
-        db: 'mongodb://scrs:R0ckOn!$$@ds011298.mlab.com:11298/scrs',
+        db: process.env.MONGODB_URI,
         rootPath: rootPath,
         port: process.env.PORT || 80
     }
