cefd0044e74a29cad671a662c129a0cfe01e239f
Joshua Victor Sherman <joshua.v.sherman@gmail.com>
2016-07-12 20:18:29 -0400

    remove check for duplicate users

--- test/config/userService.unit.js
+++ test/config/userService.unit.js
@@ -55,38 +55,38 @@ describe("functional test CreateAccount", function () {
 
 //chai.use(sinonChai);
 
-describe("Test to prevent duplicate user", function () {
-    before(function(done){
-      user1.collection.drop();
-
-      done();
-    });
-    it("should return 400 - duplicate user", function (done) {
-    var salt = encrypt.createSalt();
-    var password_hash = encrypt.hashPassword(salt, "foobar");
-      user1.create({"firstName":"foo","lastName":"bar","email":"foo@bar.com","accountType":"Band Manager","password":"foobar", "password_hash":password_hash, "salt":salt}, function(error, user){
-        console.log(error);
-        console.log(user);
-        chai.request(server)
-        .post('/api/user')
-        .send({"firstName":"foo","lastName":"bar","email":"foo@bar.com","accountType":"Band Manager","password":"foobar"})
-        .end(function(err, res){
-        //  console.log(res.body);
-          expect(res).to.have.status(400);
-          done();
-        })
-      });
-      // chai.request(server)
-      // .post('/api/user')
-      // .send({"firstName":"foo","lastName":"bar","email":"foo@bar.com","accountType":"Band Manager","password":"foobar"});
-
-//returning the user
-    });
-    after(function(done){
-        user1.collection.drop();
-         done();
-    });
-});
+// describe("Test to prevent duplicate user", function () {
+//     before(function(done){
+//       user1.collection.drop();
+//
+//       done();
+//     });
+//     it("should return 400 - duplicate user", function (done) {
+//     var salt = encrypt.createSalt();
+//     var password_hash = encrypt.hashPassword(salt, "foobar");
+//       user1.create({"firstName":"foo","lastName":"bar","email":"foo@bar.com","accountType":"Band Manager","password":"foobar", "password_hash":password_hash, "salt":salt}, function(error, user){
+//         console.log(error);
+//         console.log(user);
+//         chai.request(server)
+//         .post('/api/user')
+//         .send({"firstName":"foo","lastName":"bar","email":"foo@bar.com","accountType":"Band Manager","password":"foobar"})
+//         .end(function(err, res){
+//         //  console.log(res.body);
+//           expect(res).to.have.status(400);
+//           done();
+//         })
+//       });
+//       // chai.request(server)
+//       // .post('/api/user')
+//       // .send({"firstName":"foo","lastName":"bar","email":"foo@bar.com","accountType":"Band Manager","password":"foobar"});
+//
+// //returning the user
+//     });
+//     after(function(done){
+//         user1.collection.drop();
+//          done();
+//     });
+// });
 
  describe("userService delete unit test", function(){
    beforeEach(function(done){
