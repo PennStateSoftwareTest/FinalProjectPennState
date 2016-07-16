264a8b7f46879271d96893bcb4d5c03691b3c886
Joshua Victor Sherman <joshua.v.sherman@gmail.com>
2016-07-16 10:31:19 -0400

    filters are all working

--- server/services/bandService.js
+++ server/services/bandService.js
@@ -55,3 +55,83 @@ exports.updateBand = function() {
 exports.deleteBand = function() {
 
 };
+
+
+exports.findBands = function(request, response, next) {
+    var bandRequestData = request.body;
+    console.log(request.body);
+    var counter = 0;
+    for(var i = 0; i < 3; i++){
+      if(bandRequestData.Genre[i] != ""){
+        counter++;
+      }
+    }
+    if(counter == 1){
+      Band.find({
+          $and:[
+            {$or:[{bandGenre1: {$in:bandRequestData.Genre}}, {bandGenre2: {$in:bandRequestData.Genre}}, {bandGenre3: {$in:bandRequestData.Genre}}]},
+            {bandPayRate:{$lte:bandRequestData.Rate}}
+          ]
+      }).
+      exec(function(err, bands){
+        if(err) {
+
+            response.status(400);
+            return response.send({reason:err.toString()});
+        } else {
+            console.log(bands);
+            response.status(200);
+            response.send(bands);
+
+        }
+      });
+    }else if(counter == 2){
+      Band.find({
+        $and:[
+          {$or:
+              [
+                {$and:[{bandGenre1:{$in:bandRequestData.Genre}}, {bandGenre2:{$in:bandRequestData.Genre}}]},
+                {$and:[{bandGenre1:{$in:bandRequestData.Genre}}, {bandGenre3:{$in:bandRequestData.Genre}}]},
+                {$and:[{bandGenre2:{$in:bandRequestData.Genre}}, {bandGenre3:{$in:bandRequestData.Genre}}]},
+              ]},
+          {bandPayRate:{$lte:bandRequestData.Rate}}
+        ]
+      }).
+      exec(function(err, bands){
+        if(err) {
+
+            response.status(400);
+            return response.send({reason:err.toString()});
+        } else {
+            console.log(bands);
+            response.status(200);
+            response.send(bands);
+
+        }
+      });
+    }else if(counter == 3){
+        Band.find({
+                $and:[{bandGenre1:{$in:bandRequestData.Genre}}, {bandGenre2:{$in:bandRequestData.Genre}}, {bandGenre3:{$in:bandRequestData.Genre}},  {bandPayRate:{$lte:bandRequestData.Rate}}]
+        }).
+        exec(function(err, bands){
+          if(err) {
+
+              response.status(400);
+              return response.send({reason:err.toString()});
+          } else {
+              console.log(bands);
+              response.status(200);
+              response.send(bands);
+
+          }
+        });
+    }else{
+      Band.find({bandPayRate:{$lte:bandRequestData.Rate}}, function (err, bands){
+        if(err){
+          return response.send(err);
+        }else{
+          response.json(bands);
+        }
+      })
+    }
+};
